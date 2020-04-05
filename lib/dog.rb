5cae50736edac42d9344f694c237abce66660a25
edmundo papi <edmundopierre@yahoo.com>
2020-04-05 19:54:30 +0000

    Automatically backed up by Learn

--- lib/dog.rb
+++ lib/dog.rb
@@ -1,1 +1,37 @@
 # Add your code here
+require 'pry'
+
+#attr_accesor :name
+class Dog
+attr_accessor :name
+  @@all = []
+
+  def initialize(name)
+    @name = name
+    @@all << self
+  end
+
+  def save
+    @@all <<  self
+  end
+
+  def self.all
+    @@all.each do |dog|
+        puts dog.name
+     end
+
+     #@@all
+ binding.pry
+     #save
+  end
+
+   def self.print_all
+     @@all.each do |nam|
+         puts nam.name
+      end
+   end
+
+   def self.clear_all
+      @@all.clear
+   end
+end
